ce474386a2b04dadf07a9b2e8a1e24b94a4767bf
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-05-02 18:34:17 +0100

    Drop dependency

--- R/utils.R
+++ R/utils.R
@@ -3,8 +3,15 @@
 }
 
 
+with_dir <- function(path, expr) {
+  owd <- setwd(path)
+  on.exit(setwd(owd))
+  force(expr)
+}
+
+
 zip_dir <- function(path, dest = paste0(basename(path), ".zip")) {
-  withr::with_dir(dirname(path), {
+  with_dir(dirname(path), {
     zip::zipr(dest, basename(path))
     normalizePath(dest)
   })
